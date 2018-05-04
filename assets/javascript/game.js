a4b8903c8c766f711c8769f0e35ece3ab8a0d4f8
Aaron <aaronssy1@gmail.com>
2018-05-03 18:30:50 -0700

    first for loop to create blanks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,22 +3,33 @@ var loss = 0;
 
 // 32 animes in array
 var anime = [
-'naruto', 'one piece', 'bleach', 'hunter x hunter', 'kill la kill', 'akira', 'made in abyss', 
-'toradora', 'clannad', 'blue excorcist', 'steins gate', 'full metal alchemist', 'my hero academia', 
-'one punch man', 'tokyo ghoul', 'haikyu', 'baccano', 'erased', 'code geass', 'cowboy bebop',
-'death note', 'death parade', 'digimon', 'dragon ball', 'durarara', 'neon genesis evangelion',
-'fairy tail', 'fate stay night', 'flcl', 'gabriel dropout', 'little witch academia', 'magi',
+    'naruto', 'one piece', 'bleach', 'hunter x hunter', 'kill la kill', 'akira', 'made in abyss',
+    'toradora', 'clannad', 'blue excorcist', 'steins gate', 'full metal alchemist', 'my hero academia',
+    'one punch man', 'tokyo ghoul', 'haikyu', 'baccano', 'erased', 'code geass', 'cowboy bebop',
+    'death note', 'death parade', 'digimon', 'dragon ball', 'durarara', 'neon genesis evangelion',
+    'fairy tail', 'fate stay night', 'flcl', 'gabriel dropout', 'little witch academia', 'magi',
 ];
 
-var guessedLetters =[];
+var guessedLetters = [];
 var attemptsRemaining = 9;
 var points = 0;
 var win = true;
+var blank = [];
 
-var randomAnime = anime[Math.floor(Math.random() *anime.length)];
+var randomAnime = anime[Math.floor(Math.random() * anime.length)];
 
 //testing randomAnime
 console.log(randomAnime);
 
+for (i = 0; i < randomAnime.length; i++) {
+    
+    if (randomAnime[i] === " ") {
+        blank.push(" ");
+    } else {
+        blank.push("_");
+    }
 
+}
+
+console.log(blank);
 
