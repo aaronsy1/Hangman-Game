e7ffef6bce02bd9d8819c81b0a0a671fd1681235
Aaron <aaronssy1@gmail.com>
2018-05-06 00:47:55 -0700

    changed theme from anime to persona

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,45 +1,42 @@
 var wins = 0;
 var loss = 0;
 
-// 32 animes in array
-var anime = [
-    'naruto', 'one piece', 'bleach', 'hunter x hunter', 'kill la kill', 'akira', 'made in abyss',
-    'toradora', 'clannad', 'blue excorcist', 'steins gate', 'full metal alchemist', 'my hero academia',
-    'one punch man', 'tokyo ghoul', 'haikyu', 'baccano', 'erased', 'code geass', 'cowboy bebop',
-    'death note', 'death parade', 'digimon', 'dragon ball', 'durarara', 'neon genesis evangelion',
-    'fairy tail', 'fate stay night', 'flcl', 'gabriel dropout', 'little witch academia', 'magi',
+// 32 characters in array
+var char = [
+    'ren', 'futaba', 'ryuji', 'yusuke', 'makoto', 'haru', 'akechi', 'morgana'
 ];
 
 var guessedLetters = [];
 var attemptsRemaining = 9;
 var points = 0;
 var win = true;
-var blank =[];
+var blank = [];
+var endGame = false;
+var found = false;
 
-//determine anime from array
-var randomAnime = anime[Math.floor(Math.random() * anime.length)];
+//determine character from array
+var randomChar = char[Math.floor(Math.random() * char.length)];
 
-//testing randomAnime
-console.log(randomAnime);
+//testing randomChar
+console.log(randomChar);
 
-//create an array of underscores and spaces to "hide the random anime"
-for (i = 0; i < randomAnime.length; i++) 
-{
-    
-    if (randomAnime[i] === " ") {
+//create an array of underscores and spaces to "hide the random character"
+for (i = 0; i < randomChar.length; i++) {
+
+    if (randomChar[i] === " ") {
         blank.push(" ");
-    } 
+    }
     else {
         blank.push("_");
     }
-    
+
 }
 
 
 //function to change array of underscores and spaces into string
-var arrToStr = function(arr) {
+var arrToStr = function (arr) {
     var str = "";
-    for(var i = 0; i < arr.length; i++) {
+    for (var i = 0; i < arr.length; i++) {
         str += arr[i];
     }
     return str;
@@ -49,44 +46,81 @@ var arrToStr = function(arr) {
 empty = arrToStr(blank);
 
 //initial press that replaces text
-document.onkeyup = function (event) 
-{
+document.onkeyup = function (event) {
     //replaces "press any key to start with empty"
     document.getElementById("start-game").textContent = empty;
 
-    //key press that determines user guess and starts for loop
-    document.onkeyup = function (event) 
+    //key press that determines user guess and starts game
+    document.onkeyup = function (event) {
+        var userGuess = event.key;
+        console.log("USERGUESS: " + userGuess);
+        console.log("found: " + found);
+        if (guessedLetters.length === 0) {
+            guessedLetters.push(userGuess);
+            console.log("length =0");
+        }
+        else {
+            console.log("running for loop");
+            found = false;
+            for (var k = 0; k < guessedLetters.length; k++) {
+                
+                if (userGuess === guessedLetters[k]) {
+                    found = true;
+                    console.log("found set to true");
+
+                }
+                
+            }
+
+            if(!found){
+                guessedLetters.push(userGuess);
+            }
+        }
+
+       
+
+
+        console.log(guessedLetters);
+
+        //display guessAttempted to guessed letters
+        document.getElementById("guessed-letters").textContent = guessedLetters;
+
+        //loop that iterates through random char to replace underscore with letter
+        for (var j = 0; j < randomChar.length; j++) {
+
+
+            if (userGuess === randomChar[j]) {
+
+                //replacing blank ARRAY with userGuess
+                blank[j] = userGuess;
+                //turning blank array into string
+                empty = arrToStr(blank);
+                //displaying updated letters to screen
+                document.getElementById("start-game").textContent = empty;
+                
+                //check to make sure points increment only when userGuess is inputted the FIRST time
+                if(!found){
+                    points++;
+                }
+                console.log(points);
+
+
+
+            }
+            
+
+
+
+
+        }
+
+        if(points === randomChar.length)
         {
-            var userGuess = event.key;
-            console.log(userGuess);
+            endGame = true;
+        }
 
-    //loop that iterates through random anime to replace underscore with letter
-    for (var j =0; j<randomAnime.length; j++) 
-    {
-        
-            
-            if (userGuess === randomAnime[j]) 
-            {
-            
-            //replacing blank ARRAY with userGuess
-            blank[j]=userGuess;
-            //turning blank array into string
-            empty = arrToStr(blank);
-            //displaying updated letters to screen
-            document.getElementById("start-game").textContent = empty;
-
-            points++;
-            console.log(points);
-
-
-            
-            }
-            else
-            {
-
-            }
-
-        
+        if (endGame === true){
+            console.log("GAME ENDED");
         }
     }
 
@@ -94,15 +128,15 @@ document.onkeyup = function (event)
 
 
 
-   
-
-    
 
 
-        
 
-    
-    
+
+
+
+
+
+
 
 
 
